a6109e44d220fc6ed645a6ce112df45608545b7c
messafa <172132016926@univ-chlef.dz>
2024-12-13 20:27:15 +0100

    read and write more

--- files/file1.go
+++ files/file1.go
@@ -22,4 +22,47 @@ func main() {
 
 	fmt.Printf("Lenght: %d bytes\n", lenghth)
 	defer file.Close()
+
+	readFile("./files/file1.txt")
+	writeMore("./files/file1.txt", "\nI'm doing fine, thank you!")
+	readFile("./files/file1.txt")
 }
+
+func readFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bs := make([]byte, stat.Size())
+	_, err = file.Read(bs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	str := string(bs)
+	fmt.Println(str)
+}
+
+func writeMore(filename string, content string) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	lenght, err := file.WriteString(content)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Lenght: %d bytes\n", lenght)
+}
+
+
