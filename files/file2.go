c20222fd9eafb97341555195a0e67907240dd346
messafa <172132016926@univ-chlef.dz>
2024-12-14 12:06:55 +0100

    add file2.go to create and write user input to file2.txt

---
+++ files/file2.go
@@ -0,0 +1,36 @@
+package main 
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	file, err := os.Create("./file2.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var content string
+	fmt.Print("Enter content: ")
+	// write string with spaces 
+	fmt.Scanln(&content)
+	fmt.Println(content)
+
+
+	length, err := file.WriteString(content)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Length: %d bytes\n", length)
+
+	defer file.Close()
+
+
+
+
+}
